578a6d9f827dcaa090e322b0429d28c8f7d3a002
Daelso <11338807+Daelso@users.noreply.github.com>
2022-10-11 22:29:17 -0400

    initial commit

---
+++ server/routes/api/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+// Gets
+router.get('/', (req,res) => {
+  res.send('hello')
+})
+
+//Add
+
+//Deletes
+
+module.exports = router;
