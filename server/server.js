fba2830bf9754d59f84c8658e0bb5636d6b0759b
Chase Murtaugh <Chasemurtaugh@gmail.com>
2022-10-12 01:46:59 -0400

    Some more work on controllers

--- server/server.js
+++ server/server.js
@@ -1,53 +1,39 @@
-const express = require('express');
-const bcrypt = require('bcrypt')
+const express = require("express");
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+var cors = require("cors");
 const app = express();
 app.use(express.json());
-const port = process.env.PORT || 3000
-const userRoutes = require('./routes/api/userRoutes')
-app.use('/api/userRoutes', userRoutes)
-
+const port = process.env.PORT || 3000;
 const users = [];
 
-app.get('/', (req, res) => {
-  res.render('index.ejs')
-})
+//for dev use only, very insecure in a prod env
+if (process.env.ENV !== "prod") {
+  app.use(cors({ origin: true }));
+}
 
+//Below are various controller links
 
-app.get('/users', (req, res)=> {
-  res.json(users)
-})
+app.get("/users", async (req, res) => {
+  res.send(users);
+});
 
-app.post('/users', async (req, res) => {
-  try{
-    const salt =  await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(req.body.password, salt)
-    console.log(salt)
-    console.log(hashedPassword)
-    const user = {name: req.body.name, password: hashedPassword}
-    users.push(user)
-    res.status(201).send()
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+      age: req.body.age,
+      termsAccepted: req.body.acceptance,
+    });
+    res.status(200).send("uploaded user");
+  } catch {
+    res.status(500).send();
   }
-  catch {
-    res.status(500).send()
-  }
+  console.log(users);
+});
 
-})
-
-app.post('/users/login', async (req, res) => {
-    const user = users.find(user => user.name = req.body.name)
-    if(user == null) {
-      return res.status(400).send('Cannot find user!')
-    }
-    try{
-      if (await bcrypt.compare(req.body.password, user.password)) {
-        res.send('success')
-      }
-      else{
-        res.send('Access Denied')
-      }
-    } catch{
-      res.status(500).send('Error')
-    }
-})
-
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`));
--- src/components/registerForm.vue
+++ src/components/registerForm.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="q-pa-md" style="max-width: 400px">
-
-    <q-form
-      @submit="onSubmit"
-      @reset="onReset"
-      class="q-gutter-md"
-    >
-    <q-input
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+      <q-input
         filled
         v-model="userName"
         label="Your username *"
         hint="What do we call you?"
         lazy-rules
         :rules="[
-          val => val && val.length > 3 && val.length <= 10 || 'Please use between 3-10 characters.',
-          val => val && val.match(/^[a-zA-Z0-9]+$/) || 'No special characters allowed.'
+          (val) =>
+            (val && val.length > 3 && val.length <= 15) ||
+            'Please use between 3-15 characters.',
+          (val) =>
+            (val && val.match(/^[a-zA-Z0-9]+$/)) ||
+            'No special characters allowed.',
         ]"
       />
 
@@ -26,9 +25,15 @@
         hint="Make sure you will have long term access for account recovery!"
         lazy-rules
         :rules="[
-          val => val && val.length > 6 && val.length <= 64 || 'Please enter a valid email address.',
-          val => val && val.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) || 'Please enter a valid email address.'
-        ]"      />
+          (val) =>
+            (val && val.length > 6 && val.length <= 64) ||
+            'Please enter a valid email address.',
+          (val) =>
+            (val &&
+              val.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) ||
+            'Please enter a valid email address.',
+        ]"
+      />
 
       <q-input
         filled
@@ -37,10 +42,15 @@
         label="Your password *"
         hint="Never share this with anyone!"
         lazy-rules
-        :rules="[ val => val && val.length >= 8 && val.length <= 25 || 'We require between 8-25 characters.',
-                  val => val && (val === this.passwordVerify || this.passwordVerify === null)  || 'Passwords do not match!'
-
-      ]"
+        :rules="[
+          (val) =>
+            (val && val.length >= 8 && val.length <= 25) ||
+            'We require between 8-25 characters.',
+          (val) =>
+            (val &&
+              (val === this.passwordVerify || this.passwordVerify === null)) ||
+            'Passwords do not match!',
+        ]"
       />
 
       <q-input
@@ -50,9 +60,14 @@
         label="Type password again *"
         hint="Just making sure..."
         lazy-rules
-        :rules="[ val => val && val.length >= 8 && val.length <= 25 || 'We require between 8-25 characters.',
-                  val => val && (val === this.password || this.password === null)  || 'Passwords do not match!'
-      ]"
+        :rules="[
+          (val) =>
+            (val && val.length >= 8 && val.length <= 25) ||
+            'We require between 8-25 characters.',
+          (val) =>
+            (val && (val === this.password || this.password === null)) ||
+            'Passwords do not match!',
+        ]"
       />
 
       <q-input
@@ -63,36 +78,46 @@
         label="Your age *"
         lazy-rules
         :rules="[
-          val => val !== null && val !== '' || 'Please type your age',
-          val => val >= 18 && val < 100 || 'We accept users between the ages of 18-100'
+          (val) => (val !== null && val !== '') || 'Please type your age',
+          (val) =>
+            (val >= 18 && val < 100) ||
+            'We accept users between the ages of 18-100',
         ]"
       />
 
-      <q-toggle v-model="accept" label="I hereby certify that I am above the age of 18 and will obey site-wide rules or risk the loss of my account." />
+      <q-toggle
+        v-model="accept"
+        label="I hereby certify that I am above the age of 18 and will obey site-wide rules or risk the loss of my account."
+      />
 
       <div>
-        <q-btn label="Submit" type="submit" color="primary"/>
-        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        <q-btn label="Submit" type="submit" color="primary" />
+        <q-btn
+          label="Reset"
+          type="reset"
+          color="primary"
+          flat
+          class="q-ml-sm"
+        />
       </div>
     </q-form>
-
   </div>
 </template>
 
 <script>
-import { useQuasar } from 'quasar'
-import { ref } from 'vue'
+import { useQuasar } from "quasar";
+import { ref } from "vue";
 
 export default {
-  setup () {
-    const $q = useQuasar()
-
-    const userName = ref(null)
-    const email = ref(null)
-    const password = ref(null)
-    const passwordVerify = ref(null)
-    const age = ref(null)
-    const accept = ref(false)
+  setup() {
+    const $q = useQuasar();
+    const axios = require("axios");
+    const userName = ref(null);
+    const email = ref(null);
+    const password = ref(null);
+    const passwordVerify = ref(null);
+    const age = ref(null);
+    const accept = ref(false);
 
     return {
       userName,
@@ -102,34 +127,47 @@ export default {
       age,
       accept,
 
-      onSubmit () {
+      onSubmit() {
         if (accept.value !== true) {
           $q.notify({
-            color: 'red-5',
-            textColor: 'white',
-            icon: 'warning',
-            message: 'You need to agree to the terms of account creation first!'
-          })
-        }
-        else {
+            color: "red-5",
+            textColor: "white",
+            icon: "warning",
+            message:
+              "You need to agree to the terms of account creation first!",
+          });
+        } else {
           $q.notify({
-            color: 'green-4',
-            textColor: 'white',
-            icon: 'cloud_done',
-            message: 'Account successfully created!'
-          })
+            color: "green-4",
+            textColor: "white",
+            icon: "cloud_done",
+            message: "Account successfully created!",
+          });
+          let registryInfo = {
+            username: userName.value,
+            age: age.value,
+            email: email.value,
+            password: password.value,
+            acceptance: accept.value,
+          };
+
+          console.log(registryInfo);
+
+          axios
+            .post("https://localhost:3000/register", registryInfo)
+            .then((response) => console.log(response));
         }
       },
 
-      onReset () {
-        userName.value = null
-        email.value = null
-        password.value = null
-        passwordVerify.value = null
-        age.value = null
-        accept.value = false
-      }
-    }
-  }
-}
+      onReset() {
+        userName.value = null;
+        email.value = null;
+        password.value = null;
+        passwordVerify.value = null;
+        age.value = null;
+        accept.value = false;
+      },
+    };
+  },
+};
 </script>
