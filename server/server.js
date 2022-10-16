6c68856abb538b586878d5081e29870f2f569ff7
Chase Murtaugh <Chasemurtaugh@gmail.com>
2022-10-16 02:21:21 -0400

    Lots of cleanup

--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
+const mysql = require("mysql");
 require("dotenv").config();
-const bcrypt = require("bcrypt");
 var cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const port = process.env.PORT || 3000;
-const users = [];
+const lib = require("./lib"); //This is all custom functions
+
+//Above is library imports, below begin route imports
+
+const userRoutes = require("./api/user");
+app.use("/user", userRoutes);
+//Uses userRoutes file to handle all user related endpoints
+
+const posts = [
+  {
+    username: "Glennyballs",
+    post: "I hate this!",
+  },
+  {
+    username: "Carl22",
+    post: "I love this!",
+  },
+];
 
 //for dev use only, very insecure in a prod env, this is just to prevent CORS errors. Origin is your vue clients port
 if (process.env.ENV !== "prod") {
@@ -20,47 +36,9 @@ if (process.env.ENV !== "prod") {
 
 //Below are various controller links
 
-app.get("/users", async (req, res) => {
-  res.send(users);
-});
-
-app.post("/register", async (req, res) => {
-  try {
-    //Encrypts the password.
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    users.push({
-      id: Date.now().toString(),
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedPassword,
-      age: req.body.age,
-      termsAccepted: req.body.acceptance,
-    });
-    res.status(200).send("uploaded user");
-  } catch {
-    res.status(500).send();
-  }
-  console.log(users);
-});
-
-app.post("/login", async (req, res) => {
-  //Authenticate users
-  const user = users.find((user) => (user.username = req.body.username));
-  if (user == null) {
-    return res.status(400).send("Cannot find user!");
-  }
-  try {
-    if (await bcrypt.compare(req.body.password, user.password)) {
-      const user = { username: req.body.username }; //creates user obj for the token
-      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET); //Must be created in your env
-      res.json({ accessToken: accessToken });
-    } else {
-      res.send("Login failed!");
-    }
-  } catch {
-    res.status(500).send();
-  }
+app.get("/posts", lib.authenticateToken, (req, res) => {
+  console.log(req.user.username);
+  res.json(posts);
 });
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
