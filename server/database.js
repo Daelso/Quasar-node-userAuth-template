cc8c5fce331cfa74e7e6245f7fd7123449fdcc1f
Chase Murtaugh <Chasemurtaugh@gmail.com>
2022-10-20 13:08:58 -0400

    heroku

--- server/database.js
+++ server/database.js
@@ -1,15 +1,7 @@
-const mysql = require("mysql2");
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 //Contains connections for both generic mySQL for raw querying and the sequelize setup
-
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -26,4 +18,4 @@ sequelize
   .then(() => console.log("Sequelize connected"))
   .catch((err) => console.log(err));
 
-module.exports = { mysql: connection, sequelize };
+module.exports = { sequelize };
