449b8ae7fef566a690fad9368a9e05dd28eef3a9
Chase Murtaugh <Chasemurtaugh@gmail.com>
2022-10-20 12:01:50 -0400

    heroku tweaks

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express"),
   serveStatic = require("serve-static"),
   history = require("connect-history-api-fallback"),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 5000;
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
@@ -50,8 +50,4 @@ app.get("/posts", lib.authenticateToken, (req, res) => {
 
 app.listen(port, function () {
   console.log(`Server started on port ${port}`);
-  sql.mysql.connect((err) => {
-    if (err) throw err;
-    console.log("mySQL connected");
-  });
 });
