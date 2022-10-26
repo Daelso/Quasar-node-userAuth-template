a75007d262c795c8d1548fda82c1b94e5a29e3b4
Daelso <11338807+Daelso@users.noreply.github.com>
2022-10-26 18:36:47 -0400

    Policy pages

---
+++ src/pages/policy/privacyPolicy.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="flex flex-center"> Activating your account... </q-page>
+</template>
+
+<script>
+import { useQuasar } from "quasar";
+import { defineComponent } from "vue";
+import { useRouter } from "vue-router";
+
+export default defineComponent({
+  name: "accountActivation",
+  setup() {
+    const $q = useQuasar();
+    const axios = require("axios");
+    const router = useRouter();
+    let baseUrl = "";
+    if (window.location.href.includes("localhost")) {
+      baseUrl = "http://localhost:5000";
+    } else {
+      baseUrl = window.location.origin;
+    }
+  },
+});
+</script>
